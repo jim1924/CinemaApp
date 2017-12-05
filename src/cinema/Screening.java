61c7d87a6fdb282d07c38c71ef3878b367f61c1d
afify <afify@pc-199-27.customer.shef.opal.lan>
2017-12-05 17:32:07 +0000

    Screening adds seats

--- src/cinema/Screening.java
+++ src/cinema/Screening.java
@@ -13,6 +13,7 @@ public class Screening {
 private String date;
 private String time;
 private String title;
+public int index;
 private Seat[][] theater = new Seat[10][10];
 
 public Screening(String date, String time, String title)
@@ -21,15 +22,16 @@ public Screening(String date, String time, String title)
 	this.time=time;
 	this.title=title;
 	
-	for (int i=0; i<10; i++)
-	{
-		for (int j=0; j<10; j++)
-		{
-			theater[i][j]=new Seat();
-		}
-	}
+	
 	try {
 		addScreening();
+		for (int i=0; i<10; i++)
+		{
+			for (int j=0; j<10; j++)
+			{
+				theater[i][j]=new Seat(index,i,j);
+			}
+		}
 	} catch (IOException e) {
 		// TODO Auto-generated catch block
 		e.printStackTrace();
@@ -46,10 +48,14 @@ public void addScreening() throws IOException
 	
 	JSONObject obj = JSONUtils.getJSONObjectFromFile("./src/assets/obj.json");
 	JSONArray jsonArray = obj.getJSONArray("Screenings");
+	index = jsonArray.length();
 	JSONObject temp = new JSONObject();
+	JSONArray seats = new JSONArray();
 	temp.put("date", date);
 	temp.put("time", time);
 	temp.put("title", title);
+	temp.put("screeningID", index+1);
+	temp.put("seats", seats);
 	
 	jsonArray.put(temp);
 	
